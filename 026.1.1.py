58acd9011b9a91e00147e14af50c64cada6ef86a
Thacio Santos de Santana <59703956+ThacioSantana@users.noreply.github.com>
2020-11-14 15:10:44 -0300

    Add files via upload

---
+++ 026.1.1.py
@@ -0,0 +1,4 @@
+peso = float(input('Digite um peso: '))
+excesso = (peso - 50)
+multa = excesso * 4
+print('João pescou {} e deu {} kg a mais do permitido e agora ele vai pagar {} reais'.format(peso,excesso,multa))
---
+++ 026.py
@@ -0,0 +1,9 @@
+peso = float(input('Digite um peso: '))
+
+if peso > 50:
+    excedido = ((peso - 50)*4)
+    print('O peso excedeu o limite, voce soferá uma multa por quilo,{}'.format(excedido))
+elif peso == 50:
+    print('O peso passou raspando')
+else:
+    print('O peso está abaixo do valor estabelecido')
